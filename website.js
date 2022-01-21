5a5dc1cbe5e691ed23d862f1f3075c489918753c
Justin Pihowich <71193865+JustinPihowich@users.noreply.github.com>
2022-01-21 00:23:53 -0600

    Update website.js

--- website.js
+++ website.js
@@ -1,4 +1,4 @@
-function showHideButton() {
+function showHideButtonProjects() {
     var projects_section = document.getElementById('projects-section-sub');
     var displaySetting = projects_section.style.display;
     var show_hide_button = document.getElementById('show-hide-projects');
@@ -8,6 +8,22 @@ function showHideButton() {
         show_hide_button.innerHTML = 'Hide'
     } else {
         projects_section.style.display = 'none';
+        show_hide_button.innerHTML = 'Show';
+    }
+}
+
+// Currently just a copy of projects function
+// TODO: Create function with parameters for show-hide buttons
+function showHideButtonWork() {
+    var work_section = document.getElementById('work-section-sub');
+    var displaySetting = work_section.style.display;
+    var show_hide_button = document.getElementById('show-hide-work');
+
+    if (displaySetting == 'none') {
+        work_section.style.display = 'block';
+        show_hide_button.innerHTML = 'Hide'
+    } else {
+        work_section.style.display = 'none';
         show_hide_button.innerHTML = 'Show';
     }
 }
